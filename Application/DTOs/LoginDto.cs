7f7516fd5c9270e76db37c99e5c838bfaa3da885
JaretEduardo <jaret.gonzalez.ct59@gmail.com>
2026-02-26 16:49:08 -0600

    Correccion de bugs

--- Application/DTOs/LoginDto.cs
+++ Application/DTOs/LoginDto.cs
@@ -1,8 +1,13 @@
-﻿namespace fenixjobs_api.Application.DTOs
+﻿using System.Text.Json.Serialization;
+
+namespace fenixjobs_api.Application.DTOs
 {
     public class LoginDto
     {
+        [JsonPropertyName("usuario")]
         public string Usuario { get; set; } = string.Empty;
+
+        [JsonPropertyName("contrasenia")]
         public string Contrasenia { get; set; } = string.Empty;
     }
 }
--- Application/DTOs/RegisterDto.cs
+++ Application/DTOs/RegisterDto.cs
@@ -1,12 +1,25 @@
-﻿namespace fenixjobs_api.Application.DTOs
+﻿using System.Text.Json.Serialization;
+
+namespace fenixjobs_api.Application.DTOs
 {
     public class RegisterDto
     {
+        [JsonPropertyName("nombre")]
         public string Nombre { get; set; }
+
+        [JsonPropertyName("apellido_paterno")]
         public string? ApellidoPaterno { get; set; }
+
+        [JsonPropertyName("apellido_materno")]
         public string? ApellidoMaterno { get; set; }
+
+        [JsonPropertyName("usuario")]
         public string Usuario { get; set; }
+
+        [JsonPropertyName("contrasenia")]
         public string Contrasenia { get; set; }
+
+        [JsonPropertyName("tipo_usuario")]
         public string? TipoUsuario { get; set; }
     }
 }
